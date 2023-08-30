369976c1e1ba48b2d4d64e331960f08d3061b829
christian castillo <christiancastillog@gmail.com>
2023-08-30 18:01:06 +0200

    Worked on the connection with our API

--- app/app.py
+++ app/app.py
@@ -1,10 +1,28 @@
 import streamlit as st
+import requests
+
+url = st.secrets.url
 
 st.header("Solar panel condition classifier")
 st.text("""Upload an image to evaluate the condition of solar panels.""")
 
 # Allow users to upload images
-uploaded_file = st.file_uploader("Upload an image file:", )
+uploaded_file = st.file_uploader("Upload an image file:", type=["jpeg", "jpg", "png"])
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.read()
+    st.image(bytes_data)
+
+    files = {"file": (bytes_data)}
+    response = requests.post(url, files=files)
+
+    if response.status_code == 200:
+        result = response.json()
+        st.success("Images uploeaded succesfully!")
+        st.write("API response: ", result)
+    else:
+        st.error("Images upload failed.")
 
 # Evaluate images and retrieve the model's classification results
-st.button("Evaluate")
+# if st.button("Evaluate"):
+#     pass
