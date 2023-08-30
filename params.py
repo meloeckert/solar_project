ecb2cef00d1fe2a4dbc899024d8b9473ad167d53
Friedrich Polzin <friedrichpolzin96@gmail.com>
2023-08-30 16:59:45 +0200

    some new files and a folder for the model

---
+++ params.py
@@ -0,0 +1,10 @@
+import os
+
+
+########## VARIABLES #########
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+
+
+
+######### CONSTANTS #########
+LOCAL_REGISTRY_PATH =  os.path.join(os.getcwd(), "models")
