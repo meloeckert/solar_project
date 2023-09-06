0ec000ce663e248521987a6f24534372ac8837f5
Maria Eckert <meloeckert.emilia@gmail.com>
2023-09-06 12:36:56 +0200

    changed model in registry.py

--- solar_panel_status/dl_logic/registry.py
+++ solar_panel_status/dl_logic/registry.py
@@ -7,7 +7,7 @@ def load_cnn_model(MODEL_TARGET='local') -> Model:
     if MODEL_TARGET == "local":
         current_dir = os.path.dirname(os.path.abspath(__file__))
         # Get the latest model version name by the timestamp on disk
-        local_model_path = os.path.join(current_dir,'..','models','model_v4.h5')
+        local_model_path = os.path.join(current_dir,'..','models','model_multiclass_clean_damage_dirt_230904_statusquo.h5')
 
         if not local_model_path:
             return None
